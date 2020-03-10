d226394aaeb1b4afd2b518ee125b56ddc046aa04
MarinaCouso <marina.couso.martinez@gmail.com>
2020-03-10 10:45:29 +0100

    Final commit

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
+// Constantes
+
+const userNumber = document.querySelector('.js-userNumber');
+
+const clue = document.querySelector('.js-clue');
+
+const tryBtn = document.querySelector('.js-submit');
+
+const randomNumber = getRandomNumber();
+
+// Funciones
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * 100);
 }
-
-console.log(getRandomNumber());
-
-const userNumber = document.querySelector('.js-userNumber');
 
 function getUserNumber() {
   console.log(userNumber.value);
 }
 
-userNumber.addEventListener('change', getUserNumber);
-userNumber.addEventListener('change', isValid);
-
-const clue = document.querySelector('.js-clue');
-
-function isValid() {
-  if (userNumber.value < 1 || userNumber.value > 100) {
-    clue.innerHTML = 'Pista: El número debe estar entre 1 y 100';
-    clue.classList.add('warning');
-    return 'true';
-  } else {
-    return 'false';
-  }
-}
-console.log(isValid());
-
 function winNumber() {
-  if (userName.value === getRandomNumber) {
+  if (randomNumber === userNumber.value) {
     clue.innerHTML = 'Has ganado';
     clue.classList.add('success');
+  } else {
+    clue.innerHTML = 'Sigue probando';
   }
 }
 
-userNumber.addEventListener('change', isValid);
+function isValid() {
+  if (userNumber.value > 1 && userNumber.value < 100) {
+    winNumber();
+  } else if (userNumber.value < randomNumber) {
+    clue.innerHTML = 'El número es demasiado bajo';
+    clue.classList.add('warning');
+  } else if (userNumber.value > randomNumber) {
+    clue.innerHTML = 'El número es demasiado alto';
+    clue.classList.add('warning');
+  }
+}
+
+function isEntero() {
+  event.preventDefault();
+  if (userNumber.value % 1 === 0) {
+    isValid();
+  } else {
+    clue.innerHTML = 'Pista: El número debe estar entre 1 y 100';
+    clue.classList.add('warning');
+  }
+}
+
+// Listeners
+
+userNumber.addEventListener('keyup', getUserNumber);
+
+tryBtn.addEventListener('click', isEntero);
+
+// Consola
+console.log(getRandomNumber());
